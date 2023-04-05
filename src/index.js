bd571810e259aab8565ccebea9390f5b71b4c506
yaredtt <yaredtekle22@gmail.com>
2023-04-05 13:41:59 +0300

    clone part of the minimalist project captured in the video

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import enterImage from '../asset/enter.svg';
+
 const list = document.getElementById('list');
 const LIST = [
   {
@@ -27,11 +29,45 @@ const lists = () => {
 <i class="fa-regular fa-square"></i>
 <p class = "text">${LIST[i].description} </p>
 </div>
-<i class = "fa fa-trash-o de"</i>
-</li>`;
+<div class="svg">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+</svg>
+</div>
+</li>
+`;
 
     const position = 'beforeend';
     list.insertAdjacentHTML(position, text);
   }
 };
-window.document.addEventListener('DOMContentLoaded', lists);
+
+const loadAssets = () => {
+  const imgContainer = document.querySelector('.content .img img');
+  imgContainer.src = enterImage;
+  lists();
+};
+function removeAllTrashIcons() {
+  const items = document.querySelectorAll('.item');
+
+  if (items.length) {
+    items.forEach((item) => {
+      const svg = item.querySelector('.svg');
+      svg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+  </svg>`;
+    });
+  }
+}
+window.document.addEventListener('DOMContentLoaded', loadAssets);
+list.addEventListener('click', (e) => {
+  if (e.target.matches('li')) {
+    removeAllTrashIcons();
+    const svg = e.target.querySelector('.svg');
+    svg.innerHTML = '<i class="fas fa-trash"></i>';
+  } else if (e.target.tagName === 'P') {
+    removeAllTrashIcons();
+    const svg = e.target.parentElement.nextElementSibling;
+    svg.innerHTML = '<i class="fas fa-trash"></i>';
+  }
+});
